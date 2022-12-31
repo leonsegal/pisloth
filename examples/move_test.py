7668e04623c98ead89976d14caf9f9981b0cc701
Leon Segal <leonsegal1@gmail.com>
2022-12-31 15:48:52 +0000

    amend shake loop

--- examples/move_test.py
+++ examples/move_test.py
@@ -6,9 +6,9 @@ sloth.set_offset([0,0,0,0])
 def main():
     sloth.do_action('fall left')
     sloth.add_action('shake left', [[-10, 30, -5, 30], [0, 30, -5, 30]])
-    sloth.do_action('shake left')
+    while True:
+        sloth.do_action('shake left')
 
 if __name__ == "__main__":
-    while True:
-        main()
+    main()
 
