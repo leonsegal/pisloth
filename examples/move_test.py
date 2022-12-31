5e21cf7af339546ebfcb34063b819dc4e5cac7e5
Leon Segal <leonsegal1@gmail.com>
2022-12-31 15:46:24 +0000

    add shake

--- examples/move_test.py
+++ examples/move_test.py
@@ -4,8 +4,8 @@ sloth = Sloth([1,2,3,4])
 sloth.set_offset([0,0,0,0])
 
 def main():
-    sloth.do_action('fall left')
-
+    sloth.add_action('shake left', [[-10, 30, -5, 30], [0, 30, -5, 30]])
+    sloth.do_action('shake left')
 
 if __name__ == "__main__":
     while True:
