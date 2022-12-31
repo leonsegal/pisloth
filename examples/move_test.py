dabab7f2ec4077048c5b313bc774f42c8e0d4ca0
Leon Segal <leonsegal1@gmail.com>
2022-12-31 15:47:35 +0000

    amend shake

--- examples/move_test.py
+++ examples/move_test.py
@@ -4,6 +4,7 @@ sloth = Sloth([1,2,3,4])
 sloth.set_offset([0,0,0,0])
 
 def main():
+    sloth.do_action('fall left')
     sloth.add_action('shake left', [[-10, 30, -5, 30], [0, 30, -5, 30]])
     sloth.do_action('shake left')
 
